6e7968785aaac42bda865adc5a344a38d5ee0339
jjosegallegomesa <juan.gallegomesa@gmail.com>
2022-03-21 12:32:00 -0500

    cambios update

--- js/controlador.js
+++ js/controlador.js
@@ -1,6 +1,7 @@
 import { updateDoc , doc, collection,getDocs  } from "https://www.gstatic.com/firebasejs/9.6.2/firebase-firestore.js";
 import {db} from './firebase.js'
 import {llenarSala} from './llenado.js'
+import {editarEstudiante} from './enviarDatos.js'
 
 
 const estudiantes = await getDocs(collection(db, "estudiantes"));
@@ -14,49 +15,29 @@ contenedorFila.addEventListener("click",function(event){
 
     if(event.target.classList.contains("btn")){
 
-    
         if(event.target.parentElement.querySelector("input").value==""){
-            numeroPC="NAN"
+            numeroPC=""
         }else{
             numeroPC=event.target.parentElement.querySelector("input").value
         }
-
         if(event.target.parentElement.querySelector("textarea").value==""){
             novedad=""
         }else{
             novedad=event.target.parentElement.querySelector("textarea").value
         }
-
-
         estudianteID=event.target.parentElement.querySelector("p").textContent
 
 
         const estudiante = doc(db, "estudiantes",  estudianteID);
 
         try{
-
-            editar(estudiante,novedad,numeroPC)
+            editarEstudiante(estudiante,novedad,numeroPC)
             alert("Exito editando el computador asignado")
 
         }catch(e){
             alert("Upss intente nuevamente")
         }
        
-       
-
-        async function editar(referencia,novedad,numeroPC){
-
-            console.log(novedad)
-
-            await updateDoc(referencia, {
-                computador: numeroPC,
-                novedad:novedad
-              });
-
-        }
-        
-
-
     }
 
 })
--- js/enviarDatos.js
+++ js/enviarDatos.js
@@ -1,6 +1,13 @@
-export function enviarDatos(numero,novedad){
+import { updateDoc } from "https://www.gstatic.com/firebasejs/9.6.2/firebase-firestore.js";
 
+export async function editarEstudiante(referencia,novedad,numeroPC){
 
+   
+
+    await updateDoc(referencia, {
+        computador: numeroPC,
+        novedad:novedad
+    });
 
 
 
